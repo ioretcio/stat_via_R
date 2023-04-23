341f3ba483aec2a05af77dc039757a882c90527f
ioretcio <goncharukyou@gmail.com>
2023-04-23 22:35:28 +0300

    final

--- lab2.R
+++ lab2.R
@@ -1,6 +1,6 @@
 options(digits=2)
-
-# create dataset
+# Створення таблиці
+################################################################################
 Student <- c("John Davis", "Angela Williams", "Bullwinkle Moose",
               "David Jones", "Janice Markhammer", "Cheryl Cushing",
               "Reuven Ytzrhak", "Greg Knox", "Joel England",
@@ -11,11 +11,15 @@ English <- c(25, 22, 18, 15, 20, 28, 15, 30, 27, 18)
 roster <- data.frame(Student, Math, Science, English,
                      stringsAsFactors=FALSE)
 
-
+# Підрахунок усередненого балу
+################################################################################
 scaled <- scale(roster[,2:4])
 score <- apply(scaled, 1, mean)
 cbind(roster, score)
 
+
+# Підрахунок усередненого балу по ABCDF шкалі
+################################################################################
 y <- quantile(score, c(0.8, 0.6, 0.4, 0.2))
 roster$grade[score >= y[1]] <- "A"
 roster$grade[score < y[1] & score >= y[2]] <- "B"
@@ -24,9 +28,19 @@ roster$grade[score < y[3] & score >= y[4]] <- "D"
 roster$grade[score < y[4]] <- "F"
 roster
 
+
+# Ділення імені на складові
+################################################################################
 name <- strsplit(roster$Student," ")
 lastname <- sapply(name, "[", 2)
 firstname <- sapply(name, "[", 1)
 cbind(firstname, lastname, roster[, -1])
 
+# Сортування по імені (при однакових - по прізвищу)
+################################################################################
+roster[order(firstname, lastname),]
+
+
+# Сортування по прізвищу (при однакових - по імені)
+################################################################################
 roster[order(lastname, firstname),]
